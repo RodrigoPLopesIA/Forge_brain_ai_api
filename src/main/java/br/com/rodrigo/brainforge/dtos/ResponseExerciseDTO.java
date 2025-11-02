53790ae290dfbeebd46cb3d3b5551ecaea017a4f
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-11-02 13:39:12 -0300

    refactor: change the response question to dto

--- src/main/java/br/com/rodrigo/brainforge/dtos/ResponseExerciseDTO.java
+++ src/main/java/br/com/rodrigo/brainforge/dtos/ResponseExerciseDTO.java
@@ -4,7 +4,6 @@ import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
-import br.com.rodrigo.brainforge.entities.Question;
 import br.com.rodrigo.brainforge.enums.DifficultyEnum;
 
 public record ResponseExerciseDTO(
@@ -12,7 +11,7 @@ public record ResponseExerciseDTO(
         String theme,
         String description,
         DifficultyEnum difficulty,
-        List<Question> questions,
+        List<ResponseQuestionDTO> questions,
         Instant createdAt,
         Instant updatedAt
 ) {}
