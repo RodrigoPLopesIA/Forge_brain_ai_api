e7dc03ee0978b912b73f5105bf163690a92392f3
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-12-07 20:01:39 -0300

    feat: enhance global exception handling with MethodArgumentNotValidException support and improved error response structure

--- src/main/java/br/com/rodrigo/brainforge/config/ControllerAdviceConfig.java
+++ src/main/java/br/com/rodrigo/brainforge/config/ControllerAdviceConfig.java
@@ -1,40 +1,59 @@
 package br.com.rodrigo.brainforge.config;
 
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Http;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.com.rodrigo.brainforge.dtos.error.ResponseErrorDTO;
 import br.com.rodrigo.brainforge.exceptions.ExerciseMismatchException;
 import br.com.rodrigo.brainforge.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerAdviceConfig {
-    
-
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ResponseErrorDTO> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request) {
+    public ResponseEntity<ResponseErrorDTO> handleResourceNotFoundException(ResourceNotFoundException ex,
+            HttpServletRequest request) {
         ResponseErrorDTO errorDTO = new ResponseErrorDTO(
-            ex.getMessage(),
-            404,
-            String.valueOf(System.currentTimeMillis()),
-            request.getRequestURI(),
-            null
-        );
+                ex.getMessage(),
+                404,
+                String.valueOf(System.currentTimeMillis()),
+                request.getRequestURI(),
+                null);
         return ResponseEntity.status(404).body(errorDTO);
     }
 
     @ExceptionHandler(ExerciseMismatchException.class)
-    public ResponseEntity<ResponseErrorDTO> handleExerciseMismatchException(ExerciseMismatchException ex, HttpServletRequest request) {
+    public ResponseEntity<ResponseErrorDTO> handleExerciseMismatchException(ExerciseMismatchException ex,
+            HttpServletRequest request) {
         ResponseErrorDTO errorDTO = new ResponseErrorDTO(
-            ex.getMessage(),
+                ex.getMessage(),
+                400,
+                String.valueOf(System.currentTimeMillis()),
+                request.getRequestURI(),
+                null);
+        return ResponseEntity.status(400).body(errorDTO);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        Map<String, String> errors = new HashMap<String, String>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> 
+            errors.put(error.getField(), error.getDefaultMessage()) 
+            );
+        ResponseErrorDTO errorDTO = new ResponseErrorDTO(
+            "Validation failed",
             400,
             String.valueOf(System.currentTimeMillis()),
             request.getRequestURI(),
-            null
+            errors
         );
         return ResponseEntity.status(400).body(errorDTO);
     }
